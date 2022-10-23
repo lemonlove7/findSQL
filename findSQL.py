16040e5f1d999077eff7badada255ed74c2b113d
lemonlove7 <56328995+lemonlove7@users.noreply.github.com>
2022-10-23 10:39:09 +0800

    Add files via upload

---
+++ findSQL.py
@@ -0,0 +1,53 @@
+#coding=utf-8
+import os
+import time
+from burp import IBurpExtender
+from burp import IProxyListener
+import sys
+import socket
+
+if sys.version[0] == '2':
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+sql_dict=['?id=','?page=','?dir=','?search=','?category=','?file=','?class=','?url=','?news=','?item=','?menu=','?lang=','?name=','?ref=','?title=','?view=','?topic=','?thread=','?type=','?date=','?form=','?join=','?main=','?nav=','?region=']
+
+class BurpExtender(IBurpExtender,IProxyListener):
+    def registerExtenderCallbacks(self,callbacks):
+        self._helpers = callbacks.getHelpers()
+        callbacks.setExtensionName("FindSQL v1.0")
+        print('''
+        [+] findSQL scan is loaded
+        [+] ^_^
+        [+] #####################################
+        [+] findSQL v1.0
+        [+] author 刘一手
+        [+] team 鹏组安全
+        [+] github
+        [+] #####################################
+        [+] Please enjoy it
+        ''')
+        callbacks.registerProxyListener(self)
+    def processProxyMessage(self,messageIsRequest,message):
+        if not messageIsRequest:
+            RepReq = message.getMessageInfo()
+            url=RepReq.getUrl()
+            Rep_B = RepReq.getResponse()
+            Rep = self._helpers.analyzeResponse(Rep_B)
+            for i in sql_dict:
+                if i in str(url):
+                    if 'baidu.com' in str(url):
+                        break
+                    if 'csdn.net' in str(url):
+                        break
+                    Status_code=Rep.getStatusCode()
+                    Length=Rep.getHeaders()
+                    Lengths = "".join(Length)
+                    if 'Content-Length' in Lengths and str(Status_code)=='200':
+                        tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                        tcp_client_socket.connect(('127.0.0.1', 6666))
+                        send = str(url)
+                        tcp_client_socket.send(send.encode("utf-8"))
+                        feedback = tcp_client_socket.recv(1024)
+                        feedback.decode('utf-8')
+                        tcp_client_socket.close()
+                        break
---
+++ sql.py
@@ -0,0 +1,71 @@
+import os,time
+import socket
+import time
+import json
+import requests
+import threading
+
+
+def test():
+    while True:
+        tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        tcp_server_socket.bind(('127.0.0.1', 6666))
+        tcp_server_socket.listen(64)
+        client_socket, clientAddr = tcp_server_socket.accept()
+        recv_data = client_socket.recv(1024)
+        u = recv_data.decode('utf-8')
+        print(u)
+        client_socket.close()
+        payload = {'url': u}
+
+        resp = requests.get('http://127.0.0.1:8775/task/new')
+        taskid = resp.json()['taskid']
+
+        headers = {'Content-Type': 'application/json'}
+        if resp.json()['success']:
+            url = "http://127.0.0.1:8775/option/%s/set" % taskid
+            resp = requests.post(url, data=json.dumps(payload), headers=headers)
+            if resp.json()['success']:
+                url = "http://127.0.0.1:8775/scan/%s/start" % taskid
+                resp = requests.post(url, data=json.dumps(payload), headers=headers)
+                if resp.json()['success']:
+                    taskids.append(taskid)
+        else:
+            print("new task error")
+
+
+def st():
+    while True:
+        for taskid in taskids:
+            time.sleep(1)
+            url = "http://127.0.0.1:8775/scan/%s/status" % taskid
+            resp = requests.get(url)
+            if resp.json()['status'] != 'terminated':
+                pass
+            else:
+                url = "http://127.0.0.1:8775/scan/%s/data" % taskid
+                data = requests.get(url)
+                if data.json()['data']:
+                    member = [members.get('value') for members in data.json().get('data')]
+                    hh=member[0]
+                    existence_url=hh.get('url')
+                    existence_query=hh.get('query')
+                    print('[+] 存在注入 url:'+existence_url+' 参数:'+existence_query)
+                url = "http://127.0.0.1:8775/task/%s/delete" % taskid
+                requests.get(url)
+                taskids.remove(taskid)
+        time.sleep(3)
+
+
+
+if __name__ == '__main__':
+    taskids=[]
+    threads=[]
+    threads.append(threading.Thread(target=test))
+    threads.append(threading.Thread(target=st))
+    for t in threads:
+        t.start()
+
+
+
